3103d603210bca26844a6b89680a60ce7589c780
derwasp <nbcwasp@gmail.com>
2017-12-09 00:05:09 +0000

    AU: 1 updated - unreal-commander [skip ci] https://gist.github.com/2ab3f1550e4fba3bb189692d6777c1bc/1e6b807e03a6aa0f8fd6b1b6dc7c87fff5dec7cc

--- unreal-commander/tools/chocolateyInstall.ps1
+++ unreal-commander/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName            = 'unreal-commander'
   fileType               = 'exe'
-  url                    = 'http://unrealcommander.biz/download/evolution/uncomsetup3.57(build1260).exe'
+  url                    = 'http://x-diesel.net/download/evolution/uncomsetup3.57(build1264).exe'
 
-  checksum               = 'b3f8e21c5fbac7fbe06e771a741c3548c08543ba98d8817d91c8bff70ca2fbd1'
+  checksum               = '9e711d87fa583a4e951af1f714ddd51d04dd4a8d91b9db678df9715c238fd910'
 
   checksumType           = 'sha256'
 
