c207f93ad2d55d0ee2b42819afedfdb662f8a80d
Denys Zhuravel <denys.zhuravel@albelli.com>
2019-03-11 14:34:58 +0100

    fix the unzip location

--- eksctl/tools/chocolateyInstall.ps1
+++ eksctl/tools/chocolateyInstall.ps1
@@ -7,6 +7,6 @@ $packageArgs = @{
   url64bit               = 'https://github.com/weaveworks/eksctl/releases/download/0.1.23/eksctl_Windows_amd64.zip'
   checksum64             = 'd0edb944a15189520d407028c2eaae37aff966e6f1c10bedfd75e79e7fda3558'
   checksumType64         = 'sha256'
-  unzipLocation          = '$(Split-Path -parent $MyInvocation.MyCommand.Definition)'
+  unzipLocation          = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 }
 Install-ChocolateyZipPackage @packageArgs
