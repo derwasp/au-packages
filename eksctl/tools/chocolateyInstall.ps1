d68c219d3e727ad3e7c729bfd6a56a601c200baa
Denys Zhuravel <denys.zhuravel@albelli.com>
2019-03-11 14:52:45 +0100

    trace the install directory of the eksclt

--- eksctl/tools/chocolateyInstall.ps1
+++ eksctl/tools/chocolateyInstall.ps1
@@ -1,4 +1,6 @@
 ﻿
+$installDir = Split-Path $MyInvocation.MyCommand.Definition
+Write-Host "eksctl is going to be installed in '$installDir'"
 
 $ErrorActionPreference = 'Stop';
 
@@ -7,6 +9,6 @@ $packageArgs = @{
   url64bit               = 'https://github.com/weaveworks/eksctl/releases/download/0.1.23/eksctl_Windows_amd64.zip'
   checksum64             = 'd0edb944a15189520d407028c2eaae37aff966e6f1c10bedfd75e79e7fda3558'
   checksumType64         = 'sha256'
-  unzipLocation          = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  unzipLocation          = $installDir
 }
 Install-ChocolateyZipPackage @packageArgs
