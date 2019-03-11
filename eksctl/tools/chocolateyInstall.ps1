ce3f11a96c41ca54b05c2343830e99ddd03f14e2
Denys Zhuravel <denys.zhuravel@albelli.com>
2019-03-11 14:17:39 +0100

    eksctl

---
+++ eksctl/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿
+
+$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName            = 'eksctl'
+  url64bit               = ''
+  checksum64             = ''
+  checksumType64         = 'sha256'
+  unzipLocation          = '$(Split-Path -parent $MyInvocation.MyCommand.Definition)'
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ eksctl/update.ps1
@@ -0,0 +1,42 @@
+import-module au
+. $PSScriptRoot\..\_scripts\all.ps1
+
+$releases    = 'https://github.com/weaveworks/eksctl/releases'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+# function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+# function global:au_AfterUpdate  { Set-DescriptionFromReadme -SkipFirst 2 }
+
+function global:au_GetLatest {
+
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+    $version = $download_page.Links |
+                ?{$_.href -like "*/releases/tag*"} |
+                ?{$_.href -notlike "*/latest_release" } |
+                %{
+                        $fullUrl = $_.href
+                        $releases_tags_str = "releases/tag/"
+                        $index = $fullUrl.IndexOf($releases_tags_str)
+                        $fullIndex = $index + $releases_tags_str.Length
+                        $fullUrl.Substring($fullIndex)
+                   } |
+                Select -First 1
+
+    $url = "https://github.com/weaveworks/eksctl/releases/download/$version/eksctl_Windows_amd64.zip"
+
+    return @{
+        URL64        = $url
+        Version      = $version
+        ReleaseNotes = "$releases/tag/${version}"
+    }
+}
+
+update -ChecksumFor 64
