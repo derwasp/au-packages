1f68466897ba6f9e15407c9c67318ce246e5e95a
Denys Zhuravel <denys.zhuravel@albelli.com>
2019-03-27 09:51:36 +0100

    use the proper checksum for the dmg2img app

--- dmg2img/update.ps1
+++ dmg2img/update.ps1
@@ -7,7 +7,7 @@ function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
         }
     }
 }
