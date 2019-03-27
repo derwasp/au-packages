dda42114a20ea4517d4662d49c3b28e5ecbedb28
Denys Zhuravel <denys.zhuravel@albelli.com>
2019-03-27 09:54:10 +0100

    current dmg2img src

--- dmg2img/tools/chocolateyInstall.ps1
+++ dmg2img/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
   packageName            = 'dmg2img'
-  url                    = ''
-  checksum               = ''
+  url                    = 'http://vu1tur.eu.org/tools/dmg2img-1.6.7-win32.zip'
+  checksum               = 'c33595575b08d04ab3cd1d7bc0339fc7ffa473d5969ce29a2a206d08dc4f42a4'
   checksumType           = 'sha256'
   unzipLocation          = $installDir
 }
