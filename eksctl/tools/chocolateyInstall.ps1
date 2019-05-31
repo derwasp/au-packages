ca983b39596a0fb6aff8f01e64a5ccc065fa8c09
derwasp <nbcwasp@gmail.com>
2019-05-31 00:01:26 +0000

    AU: 1 updated - eksctl [skip ci] https://gist.github.com/2ab3f1550e4fba3bb189692d6777c1bc/b3ca56bf0b7c408d7954c52344fc1ad82dd978aa

--- eksctl/tools/chocolateyInstall.ps1
+++ eksctl/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
   packageName            = 'eksctl'
-  url64bit               = 'https://github.com/weaveworks/eksctl/releases/download/0.1.32/eksctl_Windows_amd64.zip'
-  checksum64             = '205175b22a6d44e5a6f32d3b112d999585cd5bf91546115fdb4ef1567e852d47'
+  url64bit               = 'https://github.com/weaveworks/eksctl/releases/download/0.1.33/eksctl_Windows_amd64.zip'
+  checksum64             = '9faf6fa39bc87e894c24557b8c322b218ee10cf18c80457bc62d5522c53cab0d'
   checksumType64         = 'sha256'
   unzipLocation          = $installDir
 }
